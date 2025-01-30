f6ec69a9fcca07e6235283e044129d73032185a0
rohitkumar9989 <69589867+rohitkumar9989@users.noreply.github.com>
2025-01-30 15:07:02 +0530

    Update initialize_.py
    Add the history message holders in initialize.py and change the ask question prmpt wherin you created the chunks of data to be passed to the model. Here we created the model through the chatgroq and the model which it imported was the llma

--- initialize_.py
+++ initialize_.py
@@ -5,6 +5,10 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_groq import ChatGroq
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 import re
 from dotenv import load_dotenv
 import os
@@ -16,6 +20,10 @@ os.environ["LANGCHAIN_PROJECT"]="FIRST_GENAI_APPLICATION"
 class llm_create():
     def __call__(self, *args, **kwds):
         llm=HuggingFaceEndpoint(model="meta-llama/Meta-Llama-3-8B-Instruct")
+        return llm
+
+    def subroutine(self, *args):
+        llm=ChatGroq(model="llama-3.1-8b-instant", api_key=os.getenv("GROQ_API_KEY"))
         return llm
     
 class Create_prompt_template():
@@ -125,7 +133,7 @@ class Ask_questions():
         self.dataframe=dataframe
         self.llm_model=llm_model
 
-    def create_chunks(self, system_prompt, user_prompt, end_message ,threshold=5000, model_threshold=2000):
+    def create_chunks(self, system_prompt, user_prompt, end_message,dict ,threshold=6000, model_threshold=2000):
         """
         Used for creating chunks of the dataset for the model to easily look onto the threshold
         data
@@ -133,24 +141,28 @@ class Ask_questions():
         prompt=[]
         prompt.extend(system_prompt)
         prompt.extend(user_prompt)
-        k=1
+        start=0
+        end=model_threshold
+        total=int(threshold/model_threshold)
         for i in range (int(threshold/model_threshold)):
-            prompt.append(("user", f"""Here is the {i}th chunk of the dataset: {self.dataframe[:model_threshold*k]}"""))
-            k+=1
+            prompt.extend([("user", f"""Here is the {i}th record of the dataset: Columns: {list(dict.keys())} Data corresponding to columns:{self.dataframe.iloc[start:end,:]}\n""")])
+            start+=model_threshold
+            end+=model_threshold
         prompt.extend(end_message)
 
         return prompt
 
-    def create_question (self, query):
+    def create_question (self, query, dict):
         system_prompt=[("system", "You are a Question Answering Assistant who answers the questions based on the given dataset. You will be provided with the dataset in multiple chats remember them and consider them as whole!")]
-        user_prompt=[("user", """Question: <question>{question}</question>, \n The dataset for you to use to answer is provided in the form of chunks in multiple chat instances below just after this message!\n 
+        user_prompt=[("user", "ABove given records are the full dataset!"),
+        ("user", """Question: <question>{question}</question>, \n The dataset for you to use to answer is provided in the form of chunks in multiple chat instances below just after this message!\n 
              ***Enclose the answer wihtin the <answer></answer> tag!!!!***,
              **After answering end the converstaion. I just need the answer for what i asked, thats it!!!!!**
              **Explain in detail, your answers have to be in the perspective of a Data Analyst**
              **The data would be provided in chunks below in multiple instances remember them as whole**
-             """)]
+             """),]
         end_message=[("system", "<answer>")]
-        c_prompt=self.create_chunks(system_prompt, user_prompt, end_message)
+        c_prompt=self.create_chunks(system_prompt, user_prompt, end_message, dict)
         prompt=ChatPromptTemplate.from_messages(c_prompt)
         chain=prompt | self.llm_model
         return chain.invoke({"question": query})
